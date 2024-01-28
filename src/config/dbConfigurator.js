196213dbe33ff4a7cdf05fa4634c8391ce4339b0
KristinaStrateva <117187138+KristinaStrateva@users.noreply.github.com>
2024-01-28 13:57:48 +0200

    Update dbConfigurator.js

--- src/config/dbConfigurator.js
+++ src/config/dbConfigurator.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://127.0.0.1:27017/second-hand-electronics';
+const uri = process.env.MONGO_URI;
 
 async function dbConnect() {
     await mongoose.connect(uri);
